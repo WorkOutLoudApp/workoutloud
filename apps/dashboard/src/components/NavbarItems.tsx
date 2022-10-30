9de7875b2a55485231a4477532f042279e2048ea
TRAMBO <98341055+baonguyen0703@users.noreply.github.com>
2022-10-30 03:05:50 -0700

    Update NavbarItems.tsx

--- apps/dashboard/src/components/NavbarItems.tsx
+++ apps/dashboard/src/components/NavbarItems.tsx
@@ -7,34 +7,6 @@ import { MdGroups } from 'react-icons/md'
 import { BsFillBellFill } from 'react-icons/bs'
 
 const NavbarItems = () => {
-<<<<<<< Updated upstream
-  return (
-    <div className={`flex justify-around items-center gap-[10vw] mt-2`}>
-        <Link href='/'>
-          <p>
-            <AiFillHome className='cursor-pointer' />
-          </p>
-        </Link>
-
-        <Link href='/workout'>
-          <p>
-            <IoBarbell className='cursor-pointer' />
-          </p>
-        </Link>
-
-        <Link href='/friends'>
-          <p>
-            <MdGroups className='cursor-pointer' />
-          </p>
-        </Link>
-        <Link href='/notifications'>
-          <p>
-            <BsFillBellFill className='cursor-pointer text-md' />
-          </p>
-        </Link>
-      </div>
-=======
-  const itemStyle = "grid justify-items-center py-1 cursor-pointer hover:rounded-lg hover:bg-gray-200"
   return (
     <div className={`grid grid-cols-4 justify-items-stretch items-center`}>
       <div className={itemStyle}>
@@ -61,7 +33,6 @@ const NavbarItems = () => {
         </Link>
       </div>
     </div>
->>>>>>> Stashed changes
   )
 }
 
