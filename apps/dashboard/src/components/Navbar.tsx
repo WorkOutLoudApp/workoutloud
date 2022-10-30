58e44c626cc21ff0e94dae7fd3ee089e8a7d4cf8
TRAMBO <98341055+baonguyen0703@users.noreply.github.com>
2022-10-30 03:08:31 -0700

    Update Navbar.tsx

--- apps/dashboard/src/components/Navbar.tsx
+++ apps/dashboard/src/components/Navbar.tsx
@@ -12,15 +12,9 @@ import NavbarItems from './NavbarItems'
 const Navbar = () => {
   const user = true
   return (
-<<<<<<< Updated upstream
-    <div className='flex-wrap w-full justify-between items-center border-b-2 border-gray-300 py-2 px-2 text-2xl'>
-      <div className='flex justify-between'>
-        <div className=''>
-=======
     <div className='block flex-wrap w-full justify-between items-center border-b-2 border-gray-300 py-2 px-2 text-2xl '>
       <div className='flex justify-between'>
         <div className='flex basis-1/5'>
->>>>>>> Stashed changes
           <Link href='/'>
             <p className='text-indigo-600 font-bold cursor-pointer'>
               WorkOutLoud
@@ -31,24 +25,15 @@ const Navbar = () => {
 
         {/* Home / Workout / Friends / Noti */}
         {user &&
-<<<<<<< Updated upstream
-          <div className='hidden xl:flex'>
-            <NavbarItems />
-=======
           <div className='hidden basis-3/5 justify-between xl:flex'>
             <div className='basis-full'>
               <NavbarItems />
             </div>
->>>>>>> Stashed changes
           </div>
         }
 
         {/* Menu / User / Login */}
-<<<<<<< Updated upstream
-        <div className='flex justify-end gap-[2vw]'>
-=======
         <div className='flex basis-1/5 justify-end gap-[2vw]'>
->>>>>>> Stashed changes
           {/* User / Login */}
           <div className='flex items-center gap-[2vw]'>
 
@@ -75,15 +60,9 @@ const Navbar = () => {
           </div>
 
           {/* Menu */}
-<<<<<<< Updated upstream
-          <div className='xl:hidden'>
-            <Link href='/menu'>
-              <GiHamburgerMenu className='cursor-pointer rounded-full px-1 text-4xl bg-gray-200' />
-=======
           <div className='flex items-center xl:hidden'>
             <Link href='/menu'>
               <GiHamburgerMenu className='cursor-pointer rounded-full px-1 text-3xl bg-gray-200' />
->>>>>>> Stashed changes
             </Link>
           </div>
         </div>
@@ -91,96 +70,11 @@ const Navbar = () => {
 
 
       {user &&
-<<<<<<< Updated upstream
-        <div className='flex-wrap xl:hidden'>
-          <NavbarItems />
-        </div>
-      }
-    </div>
-
-    // <div className='flex-wrap w-full justify-between items-center border-b-2 border-gray-300 py-2 px-2 text-2xl'>
-    //   <div className='flex'> 
-    //     <div className=''>
-    //       <Link href='/'>
-    //         <p className='text-indigo-600 font-bold cursor-pointer'>
-    //           WorkOutLoud
-    //         </p>
-    //       </Link>
-    //     </div>
-
-    //     {/* Menu / User / Login */}
-    //     <div className=''>
-    //       {/* User / Login */}
-    //       <div className=''>
-
-    //         {user ? (
-    //           <div className='hidden xl:block'>
-    //             <Link href='/profile'>
-    //               <Image
-    //                 src={userLogo}
-    //                 alt='user'
-    //                 width={25}
-    //                 height={25}
-    //                 className='rounded-full cursor-pointer'
-
-    //               />
-    //             </Link>
-
-    //           </div>
-
-    //         ) : (
-    //           <Link href='/login'>
-    //             <p className='font-["Roboto"] font-san text-white text-xl font-bold rounded cursor-pointer bg-indigo-500 hover:bg-indigo-600  px-2 py-1'>Login</p>
-    //           </Link>
-    //         )}
-    //       </div>
-
-    //       {/* Menu */}
-    //       <div className='xl:hidden'>
-    //         <Link href='/menu'>
-    //           <GiHamburgerMenu className='cursor-pointer rounded-full px-1 text-4xl bg-gray-200' />
-    //         </Link>
-    //       </div>
-    //     </div>
-
-    //   </div>
-
-
-    //   {/* Home / Workout / Friends / Noti */}
-    //   <div className={`flex flex-wrap justify-between`}>
-    //     <Link href='/'>
-    //       <p>
-    //         <AiFillHome className='cursor-pointer' />
-    //       </p>
-    //     </Link>
-
-    //     <Link href='/workout'>
-    //       <p>
-    //         <IoBarbell className='cursor-pointer' />
-    //       </p>
-    //     </Link>
-
-    //     <Link href='/friends'>
-    //       <p>
-    //         <MdGroups className='cursor-pointer' />
-    //       </p>
-    //     </Link>
-    //     <Link href='/notifications'>
-    //       <p>
-    //         <BsFillBellFill className='cursor-pointer text-md' />
-    //       </p>
-    //     </Link>
-    //   </div>
-
-
-    // </div>
-=======
         <div className='mt-1 xl:hidden'>
             <NavbarItems />       
         </div>
       }
     </div>
->>>>>>> Stashed changes
   )
 }
 
