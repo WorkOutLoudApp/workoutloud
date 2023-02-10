068ba07a72787b8954a694eb426df18d9f0fd788
Nickholas Boboaca <45183411+ImVispo@users.noreply.github.com>
2023-02-10 11:21:42 -0800

    feature(dashboard): init add workout, routine header tab switching

--- apps/dashboard/src/pages/workout/[routine].tsx
+++ apps/dashboard/src/pages/workout/[routine].tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth } from '@src/context/AuthProvider'
 import RoutineHeader from '@src/components/Workout/Header'
 import Login from '../login'
@@ -13,15 +13,23 @@ const routines = [
     description: 'Description',
   },
 ]
+const headerTabs = ['Exercises', 'History', 'Settings']
 const RoutinePage = () => {
   const { auth } = useAuth()
+  const [currentTab, setCurrentTab] = useState(headerTabs[0])
 
   return (
     <div className="w-full">
       {auth ? (
         <div className="space-y-3">
-          <RoutineHeader name="Routine Name" description="Description" />
-          <p>EXERCISES GO HERE</p>
+          <RoutineHeader
+            name="Routine Name"
+            description="Description"
+            tabs={headerTabs}
+            currentTab={currentTab}
+            setTab={setCurrentTab}
+          />
+          <p>{currentTab} goes here</p>
         </div>
       ) : (
         <Login />
--- apps/dashboard/src/pages/workout/index.tsx
+++ apps/dashboard/src/pages/workout/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth } from '@src/context/AuthProvider'
 import Routine from '@src/components/Workout/Routine'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus'
 import Login from '../login'
 
 const routines = [
@@ -14,12 +16,21 @@ const routines = [
   },
 ]
 const Index = () => {
+  const [isOpen, setIsOpen] = useState(true)
   const { auth } = useAuth()
 
   return (
     <div className="w-full">
       {auth ? (
         <div className="space-y-3 p-3">
+          <button
+            type="button"
+            className="rounded border border-black bg-[#d9d9d9] px-2 py-1"
+          >
+            <p>
+              <FontAwesomeIcon icon={faPlus} className="fa-md" /> Add Workout
+            </p>
+          </button>
           <div className="border-b border-black pb-5">
             <p className="text-3xl font-bold">Your Routines</p>
             <div className="grid grid-cols-2 gap-3">
