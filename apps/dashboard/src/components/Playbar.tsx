b420a04198864b87847dd0b9d4274a33a7b96a65
Charlie Truong <65761393+daseann@users.noreply.github.com>
2023-03-15 15:03:34 -0700

    playbar mockup

---
+++ apps/dashboard/src/components/Playbar.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { IoPlay, IoPause } from 'react-icons/io5'
+import { RiRewindFill } from 'react-icons/ri'
+import { AiOutlineForward } from 'react-icons/ai'
+import { useState } from 'react'
+
+const Playbar = () => {
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const handlePlayPause = () => {
+    setIsPlaying(!isPlaying)
+  }
+
+  return (
+    <div className="fixed bottom-0 left-0 right-0 flex items-center justify-center bg-gray-200 p-4 text-black">
+      <div className="absolute left-4 text-lg">Routine Name</div>
+      <div className="flex items-center space-x-4">
+        <button onClick={() => console.log('Rewind')}>
+          <RiRewindFill size="1.5em" />
+        </button>
+        <button onClick={handlePlayPause}>
+          {isPlaying ? <IoPause size="1.5em" /> : <IoPlay size="1.5em" />}
+        </button>
+        <button onClick={() => console.log('Fast-forward')}>
+          <AiOutlineForward size="1.5em" />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Playbar
