12831790b732c59c62d9aa080f6e53af58eef1b7
TRAMBO <baotramng7396@gmail.com>
2023-03-17 11:58:25 -0700

    desktop navbar light/dark mode, theme constants

---
+++ apps/dashboard/src/utils/constants/theme.tsx
@@ -0,0 +1,17 @@
+export const lightModeColors = {
+    logo: 'text-indigo-600',
+    text: 'text-white',
+    navbar_bg: 'bg-white',
+    navbar_item: 'fill-black',
+    navbar_item_bg: 'bg-gray-200',
+    navbar_item_active: '',
+}
+
+export const darkModeColors = {
+    logo: 'text-indigo-600',
+    text: 'text-white',
+    navbar_bg: 'bg-black',
+    navbar_item: 'fill-gray-300',
+    navbar_item_bg: '',
+    navbar_item_active: 'fill-indigo-600',
+}
