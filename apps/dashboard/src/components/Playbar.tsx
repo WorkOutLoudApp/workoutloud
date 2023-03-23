9e5d88a8c403647e3e8a1a1bca5a1a956573dcab
daseann <65761393+daseann@users.noreply.github.com>
2023-03-22 22:53:50 -0700

    Playbar update

--- apps/dashboard/src/components/Playbar.tsx
+++ apps/dashboard/src/components/Playbar.tsx
@@ -5,6 +5,25 @@ import { AiOutlineForward, AiFillHeart, AiOutlineHeart, AiOutlineAudio, AiFillAu
 import { useAuth } from '@src/context/AuthProvider';
 
 const Playbar = () => {
+  // Add a song object for demonstration purposes
+  const currentRoutine = {
+    routine: {
+      routinePicture: {
+        images: [
+          {
+            url: 'https://via.placeholder.com/150',
+          },
+        ],
+      },
+      exerciseName: 'Exercise Name',
+      routineName: [
+        {
+          name: 'Routine Name',
+        },
+      ],
+    },
+  };
+
   const [isPlaying, setIsPlaying] = useState(false);
   const [liked, setLiked] = useState(false);
   const [micActive, setMicActive] = useState(false);
@@ -27,8 +46,14 @@ const Playbar = () => {
       {user && (
         <div className="fixed bottom-0 left-0 right-0 flex items-center justify-center bg-gray-200 dark:bg-primary-variant-dark p-4 text-black dark:text-dark">
           <div className="absolute left-4 flex items-center space-x-2">
-            <span className="text-lg">Routine Name</span>
-            <button className= "invisible lg:visible " onClick={handleLike}>
+            <div className="w-10 h-10">
+              <img src={currentRoutine.routine.routinePicture.images[0].url} alt="exercise" />
+            </div>
+            <div>
+              <div className="font-bold">{currentRoutine.routine.exerciseName}</div>
+              <div className="text-sm">{currentRoutine.routine.routineName[0].name}</div>
+            </div>
+            <button className="invisible lg:visible" onClick={handleLike}>
               {liked ? (
                 <AiFillHeart size="1.5em" className="text-red-500" />
               ) : (
@@ -36,18 +61,23 @@ const Playbar = () => {
               )}
             </button>
           </div>
+
           <div className="flex items-center space-x-4">
-            <button onClick={() => console.log('Rewind')}>
+            <button className="invisible lg:visible" onClick={() => console.log('Rewind')}>
               <RiRewindFill size="1.5em" />
             </button>
-            <button onClick={handlePlayPause}>
+            <button className="invisible lg:visible"  onClick={handlePlayPause}>
               {isPlaying ? <IoPause size="1.5em" /> : <IoPlay size="1.5em" />}
             </button>
-            <button onClick={() => console.log('Fast-forward')}>
+            <button className="invisible lg:visible" onClick={() => console.log('Fast-forward')}>
               <AiOutlineForward size="1.5em" />
             </button>
           </div>
-          <div className="absolute right-4">
+
+          <div className="absolute right-4 flex items-center space-x-2">
+          <button className="visible lg:invisible" onClick={handlePlayPause}>
+              {isPlaying ? <IoPause size="1.5em" /> : <IoPlay size="1.5em" />}
+            </button>
             <button onClick={handleMicrophoneClick}>
               {micActive ? <AiFillAudio size="1.5em" /> : <AiOutlineAudio size="1.5em" />}
             </button>
