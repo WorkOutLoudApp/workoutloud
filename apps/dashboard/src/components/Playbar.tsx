20a0a046321ad54689be0f04a8deffbcc24b89bd
Charlie Truong <65761393+daseann@users.noreply.github.com>
2023-03-26 12:59:42 -0700

    added dark mode features back

--- apps/dashboard/src/components/Playbar.tsx
+++ apps/dashboard/src/components/Playbar.tsx
@@ -1,16 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { IoPlay, IoPause } from 'react-icons/io5';
-import { RiRewindFill } from 'react-icons/ri';
-import { AiOutlineForward, AiFillHeart, AiOutlineHeart, AiOutlineAudio, AiFillAudio } from 'react-icons/ai';
-import { useAuth } from '@src/context/AuthProvider';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { IoPlay, IoPause } from 'react-icons/io5'
+import { RiRewindFill } from 'react-icons/ri'
+import {
+  AiOutlineForward,
+  AiFillHeart,
+  AiOutlineHeart,
+  AiOutlineAudio,
+  AiFillAudio,
+} from 'react-icons/ai'
+import { useAuth } from '@src/context/AuthProvider'
+import axios from 'axios'
 
 const Playbar = () => {
-  const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [liked, setLiked] = useState(false);
-  const [micActive, setMicActive] = useState(false);
-  const { user } = useAuth();
+  const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [liked, setLiked] = useState(false)
+  const [micActive, setMicActive] = useState(false)
+  const { user } = useAuth()
+  const styleDefault = ''
+  const styleActive = 'fill-icon-active dark:fill-icon-active-dark'
 
   const [currentRoutine, setCurrentRoutine] = useState({
     routine: {
@@ -29,62 +37,69 @@ const Playbar = () => {
       ],
     },
     exercises: [],
-  });
-  
-  
-  
+  })
 
   useEffect(() => {
     async function fetchRoutines() {
       try {
-        const response = await axios.get('http://localhost:3000/routines');
+        const response = await axios.get('http://localhost:3000/routines')
         if (response.data && response.data.length > 0) {
-          setCurrentRoutine(response.data[0]);
+          setCurrentRoutine(response.data[0])
         }
       } catch (error) {
-        console.error('Error fetching routines:', error);
+        console.error('Error fetching routines:', error)
       }
     }
 
-    fetchRoutines();
-  }, []);
+    fetchRoutines()
+  }, [])
 
   const handlePlayPause = () => {
-    setIsPlaying(!isPlaying);
-  };
+    setIsPlaying(!isPlaying)
+  }
 
   const handleLike = () => {
-    setLiked(!liked);
-  };
+    setLiked(!liked)
+  }
 
   const handleMicrophoneClick = () => {
-    setMicActive(!micActive);
-  };
+    setMicActive(!micActive)
+  }
 
   const handleRewind = () => {
     if (currentExerciseIndex > 0) {
-      setCurrentExerciseIndex(currentExerciseIndex - 1);
+      setCurrentExerciseIndex(currentExerciseIndex - 1)
     }
-  };
+  }
 
   const handleFastForward = () => {
-    if (currentRoutine && currentExerciseIndex < currentRoutine.exercises.length - 1) {
-      setCurrentExerciseIndex(currentExerciseIndex + 1);
+    if (
+      currentRoutine &&
+      currentExerciseIndex < currentRoutine.exercises.length - 1
+    ) {
+      setCurrentExerciseIndex(currentExerciseIndex + 1)
     }
-  };
+  }
 
   return (
     <>
       {user && (
-        <div className="fixed bottom-0 left-0 right-0 flex items-center justify-center bg-gray-200 p-4 text-black">
+        <div className="dark:bg-background-dark fixed bottom-0 left-0 right-0 flex items-center justify-center bg-gray-200 p-4 text-black">
           {currentRoutine && currentRoutine.routine ? (
-            <div className="absolute left-4 flex items-center space-x-2">
-              <div className="w-10 h-10">
-                <img src={currentRoutine.routine.routinePicture.images[0].url} alt="exercise" />
+            <div className="absolute left-4 flex items-center space-x-2 dark:text-white">
+              <div className="h-10 w-10">
+                <img
+                  src={currentRoutine.routine.routinePicture.images[0].url}
+                  alt="exercise"
+                />
               </div>
               <div>
-                <div className="font-bold">{currentRoutine.routine.exerciseName}</div>
-                <div className="text-sm">{currentRoutine.routine.routineName[0].name}</div>
+                <div className="font-bold dark:text-white">
+                  {currentRoutine.routine.exerciseName}
+                </div>
+                <div className="text-sm dark:text-white">
+                  {currentRoutine.routine.routineName[0].name}
+                </div>
               </div>
               <button className="invisible lg:visible" onClick={handleLike}>
                 {liked ? (
@@ -96,40 +111,59 @@ const Playbar = () => {
             </div>
           ) : (
             <div className="absolute left-4 flex items-center space-x-2">
-              <div className="w-10 h-10">
-                <img src="https://via.placeholder.com/150" alt="No routine selected" />
+              <div className="h-10 w-10">
+                <img
+                  src="https://via.placeholder.com/150"
+                  alt="No routine selected"
+                />
               </div>
               <div>
-                <div className="font-bold">No routine selected</div>
+                <div className="font-bold dark:text-white">
+                  No routine selected
+                </div>
               </div>
             </div>
           )}
-  
+
           <div className="flex items-center space-x-4">
             <button className="invisible lg:visible" onClick={handleRewind}>
-              <RiRewindFill size="1.5em" />
+              <RiRewindFill className={styleActive} size="1.5em" />
             </button>
             <button className="invisible lg:visible" onClick={handlePlayPause}>
-              {isPlaying ? <IoPause size="1.5em" /> : <IoPlay size="1.5em" />}
+              {isPlaying ? (
+                <IoPause className={styleActive} size="1.5em" />
+              ) : (
+                <IoPlay className={styleActive} size="1.5em" />
+              )}
             </button>
-            <button className="invisible lg:visible" onClick={handleFastForward}>
-              <AiOutlineForward size="1.5em" />
+            <button
+              className="invisible lg:visible"
+              onClick={handleFastForward}
+            >
+              <AiOutlineForward className={styleActive} size="1.5em" />
             </button>
           </div>
-  
+
           <div className="absolute right-4 flex items-center space-x-2">
             <button className="visible lg:invisible" onClick={handlePlayPause}>
-              {isPlaying ? <IoPause size="1.5em" /> : <IoPlay size="1.5em" />}
+              {isPlaying ? (
+                <IoPause className={styleActive} size="1.5em" />
+              ) : (
+                <IoPlay className={styleActive} size="1.5em" />
+              )}
             </button>
             <button onClick={handleMicrophoneClick}>
-              {micActive ? <AiFillAudio size="1.5em" /> : <AiOutlineAudio size="1.5em" />}
+              {micActive ? (
+                <AiFillAudio className={styleActive} size="1.5em" />
+              ) : (
+                <AiOutlineAudio className={styleActive} size="1.5em" />
+              )}
             </button>
           </div>
         </div>
       )}
     </>
-  );
-  
-              }
+  )
+}
 
-export default Playbar;
+export default Playbar
